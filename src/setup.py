c09b1eec61afc2d7ad532b4a92bbad7b9d2d7327
neonbasschild <derrickdcochran@machinespirit.local>
2014-09-08 23:07:05 -0500

    3.9.5 pdf update fix

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2014 Daniele Simonetti
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2011 Daniele Simonetti
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
