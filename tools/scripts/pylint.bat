05b63067a79733fa9098ceecd9ab6d4148e77e57
OpenNingia <oppifjellet@gmail.com>
2015-08-18 09:48:55 +0200

    maintenance

--- tools/scripts/pylint.bat
+++ tools/scripts/pylint.bat
@@ -1,3 +1,4 @@
+cd ..\..\src
 call c:\python27\scripts\pylint -E --rcfile pylint.rc l5rcm.py
 call c:\python27\scripts\pylint -E --rcfile pylint.rc l5rcmcore
 call c:\python27\scripts\pylint -E --rcfile pylint.rc autoupdate
@@ -9,6 +10,7 @@ call c:\python27\scripts\pylint -E --rcfile pylint.rc dal
 call c:\python27\scripts\pylint -E --rcfile pylint.rc dialogs
 call c:\python27\scripts\pylint -E --rcfile pylint.rc models
 call c:\python27\scripts\pylint -E --rcfile pylint.rc sinks
+call c:\python27\scripts\pylint -E --rcfile pylint.rc api
 
 FOR %%f in (pylint_*.txt) do (
 	echo %%~ff
