8fe44587a605e84dc03e7147b58cef60d3bee0a2
Daniele <oppifjellet@gmail.com>
2016-04-26 14:48:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['PyQt4', 'lxml', 'asq'],
+    install_requires=['asq'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
