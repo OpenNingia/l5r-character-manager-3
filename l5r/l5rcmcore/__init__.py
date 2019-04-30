ddffd94a669dc6e16be0d6133d8792ab02b472f9
OpenNingia <oppifjellet@gmail.com>
2019-04-30 21:17:42 +0200

    localization resources were missing in deb package

--- l5r/l5rcmcore/__init__.py
+++ l5r/l5rcmcore/__init__.py
@@ -38,8 +38,8 @@ from l5r.util.settings import L5RCMSettings
 
 APP_NAME = 'l5rcm'
 APP_DESC = 'Legend of the Five Rings: Character Manager'
-APP_VERSION = '3.11.0'
-DB_VERSION = '3.11'
+APP_VERSION = '3.12.0'
+DB_VERSION = '3.12'
 APP_ORG = 'openningia'
 
 PROJECT_PAGE_LINK = 'https://github.com/OpenNingia/l5r-character-manager-3'
