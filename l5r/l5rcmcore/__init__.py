45fcbe3af09cc68dd70c986f81395bf6a471788b
Daniele <oppifjellet@gmail.com>
2025-02-24 12:42:30 +0100

    Fix PDF generation for coutier and ninja sheets - Fix issue with NPC export

--- l5r/l5rcmcore/__init__.py
+++ l5r/l5rcmcore/__init__.py
@@ -232,23 +232,19 @@ class L5RCMCore(QtWidgets.QMainWindow):
 
         # SHUGENJA/BUSHI/MONK SHEET
         if is_shugenja:
-            self.write_pdf(
-                'sheet_shugenja.pdf', exporters.FDFExporterShugenja())
-            if kiho_count > 0:
-                self.write_pdf('sheet_monk.pdf', exporters.FDFExporterMonk())
+            self.write_pdf('sheet_shugenja.pdf', exporters.FDFExporterShugenja())
         elif is_bushi:
             self.write_pdf('sheet_bushi.pdf', exporters.FDFExporterBushi())
-        elif is_samurai_monk or is_ninja:
+        elif is_ninja:
+            self.write_pdf('sheet_bushi.pdf', exporters.FDFExporterBushi())            
+        elif is_samurai_monk:
             self.write_pdf('sheet_bushi.pdf', exporters.FDFExporterBushi())
-            if kiho_count > 0:
-                self.write_pdf('sheet_monk.pdf', exporters.FDFExporterMonk())
         elif is_monk:
             self.write_pdf('sheet_monk.pdf', exporters.FDFExporterMonk())
         elif is_courtier:
-            self.write_pdf(
-                'sheet_courtier.pdf', exporters.FDFExporterCourtier())
+            self.write_pdf('sheet_bushi.pdf', exporters.FDFExporterCourtier())
 
-        if kiho_count > 0 and not(is_monk):
+        if kiho_count > 0:
             self.write_pdf('sheet_monk.pdf', exporters.FDFExporterMonk())
 
         # SPELLS
