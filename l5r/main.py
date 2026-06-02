c8bd6c82945184e77d5a567aabb972b0bd8813be
TheFactoryRecall <thefactoryrecall@protonmail.com>
2026-06-01 20:43:44 -0400

    Added ToolTips for tabs
    Update Tips and Allow for changes made for #379

--- l5r/main.py
+++ l5r/main.py
@@ -105,9 +105,14 @@ class L5RMain(AboutTabMixin, AdvancementsTabMixin, AdvanceMixin, AdviseMixin,
         self.tabs.setIconSize(QtCore.QSize(24, 24))
         tabs_icons = ['samurai', 'music', 'burn', 'powers', 'userinfo', 'book',
                       'katana', 'disk', 'text', 'bag', 'dragonball']
+
+        tabs_names = ['Primary Stats','Skills','Spells & Techniques','Kata & Kiho','Perks','Advancements','Arms & Armor',
+                      'Modifiers','Personal Info','Equipment','Settings']
+
         for i in range(0, self.num_tabs):
             self.tabs.setTabIcon(i, QtGui.QIcon(get_tab_icon(tabs_icons[i])))
             self.tabs.setTabText(i, '')
+
 
         # about = app_icon
         self.tabs.setTabIcon(self.num_tabs, QtGui.QIcon(get_app_icon_path()))
