edcdb208375bb1c52938ac08f13ff7ff59ef8b54
TheFactoryRecall <thefactoryrecall@protonmail.com>
2026-06-04 08:12:18 -0400

    Updated tab names to properly use self.tr()

--- l5r/main.py
+++ l5r/main.py
@@ -106,13 +106,22 @@ class L5RMain(AboutTabMixin, AdvancementsTabMixin, AdvanceMixin, AdviseMixin,
         tabs_icons = ['samurai', 'music', 'burn', 'powers', 'userinfo', 'book',
                       'katana', 'disk', 'text', 'bag', 'dragonball']
 
-        tabs_names = ['Character','Skills','Spells/Techniques','Kata/Kiho','Merits/Flaws','Advancements','Weapons',
-                      'Modifiers','Notes/Personal Info','Equipment','Settings']
+        tabs_names = [self.tr('Character'),
+                      self.tr('Skills'),
+                      self.tr('Spells/Techniques'),
+                      self.tr('Kata/Kiho'),
+                      self.tr('Merits/Flaws'),
+                      self.tr('Advancements'),
+                      self.tr('Weapons'),
+                      self.tr('Modifiers'),
+                      self.tr('Notes/Personal Info'),
+                      self.tr('Equipment'),
+                      self.tr('Settings')]
 
         for i in range(0, self.num_tabs):
             self.tabs.setTabIcon(i, QtGui.QIcon(get_tab_icon(tabs_icons[i])))
             self.tabs.setTabText(i, '')
-            self.tabs.setTabToolTip(i,self.tr(tabs_names[i]))
+            self.tabs.setTabToolTip(i,tabs_names[i])
 
 
         # about = app_icon
